445b9cb908462a64eebd899df2aa87c97f765a4b
Rene <100273606+ReneCherkas@users.noreply.github.com>
2025-01-24 12:17:05 +0300

    Desk-service

---
+++ desk-service/src/main/java/com/diplom/deskservice/DeskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diplom.deskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskServiceApplication.class, args);
+    }
+
+}
