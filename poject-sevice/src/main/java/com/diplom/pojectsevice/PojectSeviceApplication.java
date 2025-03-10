bd8a5fc8190b536ee6ea4bc1880fbfce02239b37
Rene <100273606+ReneCherkas@users.noreply.github.com>
2025-03-10 12:18:32 +0300

    сирвис по созданию проекта

---
+++ poject-sevice/src/main/java/com/diplom/pojectsevice/PojectSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.diplom.pojectsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojectSeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PojectSeviceApplication.class, args);
+    }
+
+}
