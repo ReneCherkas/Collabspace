54fae9023f976e1a1572774e73a04a0ad0e38363
Rene <100273606+ReneCherkas@users.noreply.github.com>
2025-05-09 20:11:58 +0300

    сервис с планером

---
+++ planer-service/src/main/java/com/diplom/planerservice/PlanerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.diplom.planerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlanerServiceApplication.class, args);
+    }
+}
